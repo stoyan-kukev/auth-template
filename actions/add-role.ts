d362fdef62e7c957fe9c10a856ecf7fb771e2229
stoyan-kukev <stoykukev@proton.me>
2025-02-21 15:47:20 +0200

    fix dumbass bulshit 2

--- actions/add-role.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { db } from "@/lib/db";
-import { roleTable } from "@/lib/db/schema";
-
-export async function addRole(name: string) {
-	await db.insert(roleTable).values({ name });
-}
